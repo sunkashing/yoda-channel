6ae5ee69a4885dc505c82fc83d27fa3fc2faaf4f
sunkashing <sunjiacheng1111@gmail.com>
2020-05-18 08:20:13 -0400

    部署第一版

--- webapps/settings.py
+++ webapps/settings.py
@@ -84,12 +84,23 @@ WSGI_APPLICATION = 'webapps.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+env = env('ENV')
+if env == "server":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'yoda_channel',
+            'USER': 'root',
+            'PASSWORD': env('MYSQL_ROOT_PASSWORD'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
